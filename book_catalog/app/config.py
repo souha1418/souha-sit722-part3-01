a7d5d524e327c0e8b81de903cdddbc4c18ef1b5b
souha1418 <127913869+souha1418@users.noreply.github.com>
2024-08-30 17:19:36 +1000

    Add final changes and updates

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,4 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+#DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722_part3_user:MdwAsc0tjC0B20e0DCkvmjWyeAIxdeGR@dpg-cr7rncrtq21c739euie0-a.oregon-postgres.render.com/sit722_part3')
--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+#SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://sit722_part3_user:MdwAsc0tjC0B20e0DCkvmjWyeAIxdeGR@dpg-cr7rncrtq21c739euie0-a.oregon-postgres.render.com/sit722_part3" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
